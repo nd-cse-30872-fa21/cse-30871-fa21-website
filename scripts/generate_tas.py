0aeb708ace3b325c892ce7ed6393a09c8920cd4d
Peter Bui <pbui@nd.edu>
2021-08-20 10:32:37 -0400

    Handle conflicts of interest

--- scripts/generate_tas.py
+++ scripts/generate_tas.py
@@ -3,18 +3,35 @@
 import csv
 import random
 import glob
+import sys
 import yaml
 
 TAS      = [ta['github'] for ta in yaml.safe_load(open('static/yaml/ta.yaml'))]
 STUDENTS = []
 
-for student in csv.DictReader(open('static/csv/students.csv', 'rU')):
+for student in csv.DictReader(open('static/csv/students.csv', 'r')):
     STUDENTS.append(student['Netid'])
 
-random.shuffle(STUDENTS)
-random.shuffle(TAS)
+CONFLICTS = dict(
+    (ta['github'], ta['conflicts'])
+    for ta in yaml.safe_load(open('static/yaml/ta.yaml'))
+    if ta.get('conflicts')
+)
 
-TAS      = TAS * (len(STUDENTS) // len(TAS) + 1)
-MAPPING  = list(sorted(map(list, zip(STUDENTS, TAS))))
+HAS_CONFLICTS = True
+
+
+while HAS_CONFLICTS:
+    random.shuffle(STUDENTS)
+    random.shuffle(TAS)
+
+    TAS           = TAS * (len(STUDENTS) // len(TAS) + 1)
+    MAPPING       = list(sorted(map(list, zip(STUDENTS, TAS))))
+    HAS_CONFLICTS = False
+
+    for ta, conflicts in CONFLICTS.items():
+        for conflict in conflicts:
+            if [conflict, ta] in MAPPING:
+                HAS_CONFLICTS = True
 
 print(yaml.dump(MAPPING, default_flow_style=False))
